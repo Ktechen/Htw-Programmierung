b736fd6bda2484b68065965dca734e36800b89c6
Ktechen <57128890+Ktechen@users.noreply.github.com>
2020-04-10 00:59:29 +0200

    Add files via upload

--- Programmierung 2/HA. 1 VektorMatrix/Arrayelement.java
+++ Programmierung 2/HA. 1 VektorMatrix/Arrayelement.java
@@ -6,46 +6,67 @@ public class Arrayelement {
 
 	public byte[] haeufigsteElem(byte[] arr) throws NullPointerException {
 
-		if(arr == null) {
+		if (arr == null) {
 			throw new NullPointerException("Alter Arr is voll null und so");
 		}
-		
+
 		int count = 1;
-		//byte[] newArr = new byte[arr.length]; //anzahl der elemente
-		byte[] newArrElem = new byte[arr.length]; //Die Elemente selbst
+		int index = 0;
+
+		byte[] newArr = new byte[arr.length]; // anzahl der elemente
+		byte[] newArrElem = new byte[arr.length]; // Die Elemente selbst
 
 		// Sort
 		Arrays.sort(arr);
 
-		//search element frequency
-		int index = 0;
-		for (int i = 1; i < arr.length; i++) {
-			if (arr[i] == arr[i - 1]) {
-				count++; 
-				newArrElem[index] = arr[i]; 
-				//newArr[index] = (byte) count;
-			}
+		// print arr sort
+		for (int i = 0; i < arr.length; i++) {
+			System.out.print(arr[i] + " ");
+		}
+		System.out.println();
+		System.out.println("----------------------------------------");
 
-			if (arr[i] != arr[i - 1]) {
+		// search element frequency more then two
+		for (int i = 0; i < arr.length - 1; i++) {
+
+			if (arr[i] == arr[i + 1]) {
+				count++;
+				newArrElem[index] = arr[i];
+				newArr[index] = (byte) count;
+			}
+			
+			if (arr[i] != arr[i + 1]) {
 				count = 1;
-				index++; 
+
+				// filter first element if count == 1
+				if (newArr[0] != 0) {
+					index++;
+				}
 			}
 		}
 
-		//Create new Array
+		// print count of element
+		for (int i = 0; i < newArr.length; i++) {
+			System.out.print(newArr[i] + " ");
+		}
+		System.out.println();
+
+		// Create new Array
 		int lenArr = 0;
-		for (int i = 0; i < newArrElem.length; i++) {
-			if(newArrElem[i] != 0) {
+		for (int i = 0; i < newArr.length; i++) {
+			if (newArr[i] != 0) {
 				lenArr = i + 1;
 			}
 		}
-		
-		//add Elements so Array
+
+		// add Elements so Array
 		byte[] array = new byte[lenArr];
 		for (int i = 0; i < array.length; i++) {
-			array[i] = newArrElem[i];
+			if (newArrElem[i] != 0) {
+				array[i] = newArrElem[i];
+			}
 		}
-		
+
 		return array;
 	}
 }
