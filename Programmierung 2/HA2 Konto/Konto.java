ba37d173c989ec5f44db9a3ac2046440d88ef621
Ktechen <57128890+Ktechen@users.noreply.github.com>
2020-04-17 18:46:41 +0200

    Add files via upload

--- Programmierung 2/HA2 Konto/Konto.java
+++ Programmierung 2/HA2 Konto/Konto.java
@@ -4,54 +4,88 @@ package HA2;
 public class Konto {
 
 	private double money;
-	private double startMoney = 100;
 	private final int MAX_TRANSFER_MONEY = 10000;
 	private final int MIN_TRANSFER_MONEY = 1;
 	private boolean dispo = false;
 
+	public boolean isDispo() {
+		return dispo;
+	}
+
+	public void setDispo(boolean dispo) {
+		this.dispo = dispo;
+	}
+	
 	public double getMoney() {
 		return money;
 	}
 
+	public Konto () {
+		
+	}
+	
+	public Konto(double money) {
+		this.money = money;
+	}
+	
+	public Konto(double money, boolean dispo) {
+		this.money = money;
+		this.dispo = dispo;
+	}
+	
 	/*
 	 * Geld einzahlen
-	 * 
-	 * @return boolean
 	 */
 	public void einzahlen(double transfer) {
 		if (transfer >= MIN_TRANSFER_MONEY && transfer <= MAX_TRANSFER_MONEY) {
 			this.money += transfer;
 		}
 	}
+	
+	/*
+	 * With Exception
+	 */
+	
+	/*
+	public void einzahlen(double transfer) throws IllegalArgumentException{
+		if(transfer < 0) {
+			throw new IllegalArgumentException("Transfer ist kleine 0");
+		}
+		
+		this.money += transfer;
+	}
+	*/
 
 	/*
 	 * Geld abheben
-	 * 
-	 * @return boolean
 	 */
 	public void abheben(double transfer) {
+			
 		// Limit
 		if (transfer >= MIN_TRANSFER_MONEY && transfer <= MAX_TRANSFER_MONEY) {
 
 			// transfer sum kleiner als money
-			if (transfer < this.money) {
-				this.money -= transfer;
-			}
-
-			// transfer sum kleiner als money
-			if (transfer > this.money) {
+			if (transfer >= this.money) {
 				if (dispo) {
 					this.money -= transfer;
 				}
 			}
+			
+			// transfer sum kleiner als money
+			if (transfer <= this.money) {
+				this.money = this.money - transfer;
+				//1000 = 3000 - 100;
+			}
+
 		}
+		
 	}
 
 	/*
 	 * Check ob das Konto leer ist
 	 */
-	public boolean kontoIstLeer(Konto konto) {
-		if (konto.money == 0) {
+	public boolean kontoIstLeer() {
+		if (this.money == 0) {
 			return true;
 		}
 		return false;
@@ -60,8 +94,8 @@ public class Konto {
 	/*
 	 * Clean konto
 	 */
-	public void kontoLeeren(Konto konto) {
-		double money = konto.money;
-		konto.money -= money;
+	public void kontoLeeren() {
+		double money = this.money;
+		this.money -= money;
 	}
 }
--- Programmierung 2/HA2 Konto/TestKonto.java
+++ Programmierung 2/HA2 Konto/TestKonto.java
@@ -7,19 +7,16 @@ import org.junit.Test;
 public class TestKonto {
 
 	private Konto konto;
-	
-	//private double zero = 0;
+
+	// private double zero = 0;
 	private double max = 10000;
 	private double min = 1;
 	private double money = 5000;
-	
+
 	/*
-	@Test
-	public void TestEinzahlenZero() {
-		konto = new Konto();
-		assertTrue(konto.einzahlen(zero));
-	}
-	*/
+	 * @Test public void TestEinzahlenZero() { konto = new Konto();
+	 * assertTrue(konto.einzahlen(zero)); }
+	 */
 	
 	@Test
 	public void TestEinzahlen() {
@@ -27,24 +24,24 @@ public class TestKonto {
 		konto.einzahlen(money);
 		assertTrue(konto.getMoney() == money);
 	}
-	
+
 	@Test
 	public void TestEinzahlenMin() {
 		konto = new Konto();
 		konto.einzahlen(min);
-		
-		//System.out.println(konto.getMoney());
-		
+
+		// System.out.println(konto.getMoney());
+
 		assertTrue(konto.getMoney() == min);
 	}
-	
+
 	@Test
 	public void TestEinzahlenMax() {
 		konto = new Konto();
 		konto.einzahlen(max);
 		assertTrue(konto.getMoney() == max);
 	}
-	
+
 	@Test
 	public void TestAbheben() {
 		konto = new Konto();
@@ -52,53 +49,56 @@ public class TestKonto {
 		konto.abheben(4500);
 		assertTrue(konto.getMoney() == 500);
 	}
-	
+
 	@Test
 	public void TestAbhebenMin() {
 		konto = new Konto();
 		konto.einzahlen(money);
 		konto.abheben(min);
-		assertTrue(konto.getMoney() == money-1);
+		assertTrue(konto.getMoney() == money - 1);
 	}
-	
+
 	@Test
-	public void TestAbhebenMax() {
+	public void TestAbhebenMaxBlock() {
 		konto = new Konto();
 		konto.einzahlen(money);
-		konto.abheben(max);
-		assertTrue(konto.getMoney() == 0);
+		
+		double currentMoney = konto.getMoney();
+		
+		konto.abheben(10000);
+		assertTrue(konto.getMoney() == currentMoney);
+		
 	}
-	
+
 	@Test
 	public void TestKontoIstLeer() {
 		konto = new Konto();
-		assertTrue(konto.kontoIstLeer(konto));
+		assertTrue(konto.kontoIstLeer());
 	}
-		
+
 	@Test
 	public void TestKontoIstNichtLeer() {
 		konto = new Konto();
-		konto.einzahlen(2000);
-		
-		if(!konto.kontoIstLeer(konto)) {
+		konto.einzahlen( 2000);
+
+		if (!konto.kontoIstLeer()) {
 			assertTrue(true);
 		}
-		
+
 	}
-	
+
 	@Test
 	public void TestCleanKonto() {
 		konto = new Konto();
 		double money = konto.getMoney();
-		
-		if(money < 0) {
+
+		if (money <= 0) {
 			konto.einzahlen(2000);
 		}
-		
-		//konto.kontoLeeren(konto);
+
+		konto.kontoLeeren();
 		
 		assertTrue(konto.getMoney() == 0);
 	}
-	
-	
+
 }
