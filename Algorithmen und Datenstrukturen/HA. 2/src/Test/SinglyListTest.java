7c15cdd6dae4bf7a81ddf832f4faa5482779b857
Ktechen <57128890+Ktechen@users.noreply.github.com>
2020-05-01 03:45:58 +0200

    Add files via upload

---
+++ Algorithmen und Datenstrukturen/HA. 2/src/Test/SinglyListTest.java
@@ -0,0 +1,65 @@
+package Test;
+
+import static org.junit.Assert.*;
+
+import org.junit.Test;
+
+import data.Student;
+import list.SinglyLindedList;
+
+public class SinglyListTest {
+
+	@Test
+	public void PrintAll() {
+		
+		Student s1 = new Student("Mosby", "Ted", 1, 100);
+		Student s2 = new Student("Donald", "Duck", 56, 55);
+		Student s3 = new Student("Cooper", "Shelden", 1, 999);
+		Student s4 = new Student("Alice", "Cooper", 56, 11);
+		
+		SinglyLindedList<Student> singlyLindedList = new SinglyLindedList<Student>();
+		singlyLindedList.add(s1);
+		singlyLindedList.add(s2);
+		singlyLindedList.add(s3);
+		singlyLindedList.add(s4);
+		
+		
+		singlyLindedList.printAll();
+		
+	}
+	
+	@Test
+	public void addElement() {
+		
+		Student s1 = new Student("Mosby", "Ted", 1, 100);
+
+		SinglyLindedList<Student> singlyLindedList = new SinglyLindedList<Student>();
+		singlyLindedList.add(s1);
+		
+		assertNotNull(singlyLindedList.get(0));
+	}
+	
+	@Test
+	public void addFirstElement() {
+		
+		Student s0 = null;
+		Student s1 = new Student("Mosby", "Ted", 1, 100);
+		Student s2 = new Student("Donald", "Duck", 56, 55);
+		Student s3 = new Student("Cooper", "Shelden", 1, 999);
+		Student s4 = new Student("Alice", "Cooper", 56, 11);
+		
+		SinglyLindedList<Student> list = new SinglyLindedList<Student>();
+		list.add(s1);
+		list.add(s2);
+		list.add(s3);
+		list.add(s4);
+		
+		System.out.println();
+		
+		list.addFirst(s0);
+		
+		list.printAll();
+	}
+
+	
+}
