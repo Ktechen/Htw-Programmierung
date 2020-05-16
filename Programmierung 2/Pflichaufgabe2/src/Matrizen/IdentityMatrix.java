cb69a298bc3de0b64200e2c28e46862e8c2141cb
Ktechen <57128890+Ktechen@users.noreply.github.com>
2020-05-17 01:18:50 +0200

    Add files via upload

--- Programmierung 2/Pflichaufgabe2/src/Matrizen/IdentityMatrix.java
+++ Programmierung 2/Pflichaufgabe2/src/Matrizen/IdentityMatrix.java
@@ -6,12 +6,6 @@ public class IdentityMatrix extends QuadraticMatrix {
 		super(matrix);
 
 		for (int i = 0; i < matrix.length; i++) {
-			if (matrix[i][i] != 1) {
-				throw new IllegalArgumentException("Es besteht keine Einheitsmatrix");
-			}
-		}
-
-		for (int i = 0; i < matrix.length; i++) {
 			for (int e = 0; e < matrix[i].length; e++) {
 				if (!(matrix[i][e] == 0 || matrix[i][e] == 1 && (i + e) % 2 == 0)) {
 					throw new IllegalArgumentException("Es besteht keine Einheitsmatrix");
